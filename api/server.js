7ac6a4bffcf24ee8e253c28ef9f5fb247ef2762d
unknown <gradybollas@gmail.com>
2021-03-19 08:48:36 -0700

    index and server fleshed out

--- api/server.js
+++ api/server.js
@@ -1,1 +1,21 @@
 // build your server here and require it from index.js
+const express = require('express')
+const helmet = require('helmet')
+
+const db = require('../data/dbConfig')
+
+const ProjectRouter = require('./project/router')
+const ResrouceRouter = require('./resource/router')
+const TaskRouter = require('./task/router')
+
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+
+
+server.use('/api/resources', ResrouceRouter)
+server.use('/api/projects', ProjectRouter)
+server.use('/api/task', TaskRouter)
+
+module.server;
